bf3abe987eb1b5813e71d740fba23ab1de36df10
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-11 23:57:32 -0400

    created key value pairs in month conversions

---
+++ main.py
@@ -0,0 +1,14 @@
+month_Conversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
